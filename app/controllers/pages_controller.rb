827f88db63221c1f189e4cc33a462c5e69dd9dab
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-11 15:49:06 -0400

    kittens

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,17 @@ class PagesController < ApplicationController
   @header = "This is the welcome page!!!"
   end
 
+  def contest
+  @header = "This is the contest page!!!"
+  end
+
+  def about
+  @header = "This is the about page!!!"
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
 end
